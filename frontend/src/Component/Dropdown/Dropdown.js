16212256bb3b70767fa61c5094b7ed7451fc8164
Jorge Betancur <jbetancur@elespectador.com>
2022-05-30 16:53:13 +0000

    feat(dropdown): created dropdown component

---
+++ frontend/src/Component/Dropdown/Dropdown.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import './Dropdown.css'
+
+export default function Dropdown(props) {
+  return (
+    <div className="dropdown">
+      <button className="dropbtn">{props.name}</button>
+      <div className="dropdown-content">
+        {props.data.map((prop) => (
+          <Link to="/" key={prop.id} to={prop.url} className={prop.class || ''}>
+            {prop.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
