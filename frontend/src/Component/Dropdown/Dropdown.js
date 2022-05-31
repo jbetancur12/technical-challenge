7f2be096cd404682590cd25761596f37337a1369
Jorge Betancur <jbetancur@elespectador.com>
2022-05-31 01:35:15 +0000

    style(dropdownstyles): added chevon icon with styles

--- frontend/src/Component/Dropdown/Dropdown.js
+++ frontend/src/Component/Dropdown/Dropdown.js
@@ -7,6 +7,14 @@ export default function Dropdown(props) {
   return (
     <div className="dropdown">
       <button className="dropbtn">{props.name}</button>
+      <svg
+        x="0"
+        y="0"
+        viewBox="0 0 14 8"
+        fill="none"
+        class="dropdown-iconChevon">
+        <path d="M1,1l6.2,6L13,1"></path>
+      </svg>
       <div className="dropdown-content">
         {props.data.map((prop) => (
           <Link key={prop.id} to={prop.url} className={prop.class || ''}>
